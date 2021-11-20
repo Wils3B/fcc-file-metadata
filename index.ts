013bed03b4bc8d7c177e8410c129de162ceca9f8
Wilson Gouanet <wilstito@gmail.com>
2021-11-20 11:16:39 -0500

    Add index page with upload form

--- index.ts
+++ index.ts
@@ -10,11 +10,8 @@ async function bootstrap() {
   app.use(cors());
   app.use(morgan(config.app.isProd ? 'common' : 'dev'));
 
-  app.get('/', (req, res) => {
-    res.send({ success: true, message: 'Welcome' });
-  });
-
   app.use('/api/fileanalyse', FileRouter);
+  app.use('/', express.static('public'));
 
   app.listen(config.app.port, () => {
     console.log('⚡️[server]: Server is running on port %s', config.app.port);
